61bfe78916e14a6c4a6e92e219f2b85833fe612e
Asel Zh <assollzh@gmail.com>
2022-07-19 16:47:10 -0700

    updated pencilkit

---
+++ IWish/IWish/ViewModels/DrawingView/ItemDrawingView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDrawingView.swift
+//  IWish
+//
+//  Created by Viktoriya Kudryashova on 7/19/22.
+//
+
+import SwiftUI
+
+struct ItemDrawingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDrawingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDrawingView()
+    }
+}
